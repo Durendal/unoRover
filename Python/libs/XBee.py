3d2d7d85b9c8112e5353c0a009f71ed68152173e
Durendal <sir.durendal@gmail.com>
2014-12-04 00:10:55 -0600

    Updated to include new message protocol

--- Python/libs/XBee.py
+++ Python/libs/XBee.py
@@ -10,6 +10,18 @@ class XBee:
 	   	self.name = 'XBee'
 	   	self.description = 'Library to manage XBee communications'
 	   	self.reading = 0
+	   	self.commands = { 'ENDMSG' : 0x00, 
+	   					  'FWD' : 0x01, 
+	   					  'BAC' : 0x02,
+	   					  'STP' : 0x03,
+	   					  'LFT' : 0x04,
+	   					  'RGT' : 0x05,
+	   					  'GSP' : 0x06,
+	   					  'SPD' : 0x07,
+	   					  'READ' : 0x08,
+	   					  'TYPS' : 0x09,
+	   					  'NUMS' : 0x0A
+	   					}
 				
 
 	def setup(self, ser):
@@ -33,14 +45,30 @@ class XBee:
 		
 	def sendCmd(self, cmd):
 		if cmd == "FWD":
-			self.serial.write("COMMAND: WRIT MOVE FWD")
+			self.serial.write(self.commands['FWD'])
 		elif cmd == "BAC":
-			self.serial.write("COMMAND: WRIT MOVE BAC")
+			self.serial.write(self.commands['BAC'])
 		elif cmd == "LFT":
-			self.serial.write("COMMAND: WRIT TURN LFT")
+			self.serial.write(self.commands['LFT'])
 		elif cmd == "RGT":
-			self.serial.write("COMMAND: WRIT TURN RGT")
+			self.serial.write(self.commands['RGT'])
 		elif cmd == "STP":
-			self.serial.write("COMMAND: WRIT MOVE STP")
-		elif "READ" in cmd or "TYPE" in cmd or "NUMS" in cmd:
-			self.serial.write("COMMAND: "+cmd)
+			self.serial.write(self.commands['STP'])
+		elif cmd == "NUMS":
+			self.serial.write(self.commands['NUMS'])
+		elif "READ" in cmd:
+			cmd = cmd.split(" ")
+			self.serial.write(self.commands['READ'])
+			self.serial.write(cmd[1])
+			self.serial.write(cmd[2])
+		elif "TYPS" in cmd:
+			cmd = cmd.split(" ")
+			self.serial.write(self.commands['TYPS'])
+			self.serial.write(cmd[1])
+		elif "GSP" in cmd:
+			self.serial.write(self.commands['GSP'])
+		elif "SPD" in cmd:
+			cmd = cmd.split(" ")
+			self.serial.write(self.commands['SPD'])
+			self.serial.write(cmd[1])
+		self.serial.write(self.commands['ENDMSG'])
